16fdb7468f805a2253c3b183758d07674c8a67a0
BarbecueNote <tbrothers076@gmail.com>
2023-09-27 12:17:30 +0200

    TP 2: graphQL

---
+++ UE-AD-A1-MIXTE-main/user/user.py
@@ -0,0 +1,58 @@
+# REST API
+from flask import Flask, render_template, request, jsonify, make_response
+import requests
+import json
+from werkzeug.exceptions import NotFound
+
+'''
+# CALLING gRPC requests
+import grpc
+from concurrent import futures
+import booking_pb2
+import booking_pb2_grpc
+import movie_pb2
+import movie_pb2_grpc
+'''
+# graphQL
+from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
+from ariadne.constants import PLAYGROUND_HTML
+import resolvers as r
+
+# CALLING GraphQL requests
+# todo to complete
+
+app = Flask(__name__)
+
+PORT = 3004
+HOST = '0.0.0.0'
+
+with open('{}/data/users.json'.format("."), "r") as jsf:
+   users = json.load(jsf)["users"]
+
+
+type_defs = load_schema_from_path('user.graphql')
+query = QueryType()
+user = ObjectType('User')
+query.set_field('user_with_id', r.user_with_id)
+schema = make_executable_schema(type_defs, user, query)
+
+
+@app.route('/graphql', methods=['GET'])
+def playground():
+    return PLAYGROUND_HTML, 200
+    
+@app.route('/graphql', methods=['POST'])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(
+                        schema,
+                        data,
+                        context_value=None,
+                        debug=app.debug
+                    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+if __name__ == "__main__":
+   print("Server running in port %s"%(PORT))
+   app.run(host=HOST, port=PORT)
