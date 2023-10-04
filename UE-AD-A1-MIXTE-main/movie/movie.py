61154eca950f7b3c55ad02fff8c3571542d22262
Julien-DAI <julien.dai@imt-atlantique.net>
2023-10-04 10:43:26 +0200

    tp 2 suite

--- UE-AD-A1-MIXTE-main/movie/movie.py
+++ UE-AD-A1-MIXTE-main/movie/movie.py
@@ -1,6 +1,6 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
 
@@ -9,15 +9,7 @@ HOST = '0.0.0.0'
 app = Flask(__name__)
 
 # todo create elements for Ariadne
-type_defs = load_schema_from_path('movie.graphql')
-query = QueryType()
-movie = ObjectType('Movie')
-query.set_field('movie_with_id', r.movie_with_id)
-actor = ObjectType('Actor')
-movie.set_field('actors', r.resolve_actors_in_movie)
-mutation = MutationType()
-mutation.set_field('update_movie_rate', r.update_movie_rate)
-schema = make_executable_schema(type_defs, movie, query, mutation, actor)
+
 
 # root message
 @app.route("/", methods=['GET'])
@@ -26,6 +18,23 @@ def home():
 
 #####
 # graphql entry points
+type_defs = load_schema_from_path('movie.graphql')
+query = QueryType()
+
+movie = ObjectType('Movie')
+query.set_field('movie_with_id', r.movie_with_id)
+query.set_field('movie_with_title', r.movie_with_title)
+actor = ObjectType('Actor')
+movie.set_field('actors', r.resolve_actors_in_movie)
+
+mutation = MutationType()
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+mutation.set_field('update_movie_title', r.update_movie_title)
+mutation.set_field('create_movie', r.create_movie)
+mutation.set_field('delete_movie', r.delete_movie)
+
+
+schema = make_executable_schema(type_defs, movie, query, mutation, actor)
 
 @app.route('/graphql', methods=['GET'])
 def playground():
@@ -45,4 +54,4 @@ def graphql_server():
 
 if __name__ == "__main__":
     print("Server running in port %s"%(PORT))
-    app.run(host=HOST, port=PORT)
+    app.run(host=HOST, port=PORT, debug=True)
--- UE-AD-A1-MIXTE-main/movie/resolvers.py
+++ UE-AD-A1-MIXTE-main/movie/resolvers.py
@@ -1,12 +1,24 @@
 import json
 
+from graphql import GraphQLError
+
+
 def movie_with_id(_,info,_id):
     with open('{}/data/movies.json'.format("."), "r") as file:
         movies = json.load(file)
         for movie in movies['movies']:
             if movie['id'] == _id:
                 return movie
-            
+        raise GraphQLError("bad input parameter")
+
+def movie_with_title(_,info,_title):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        for movie in movies['movies']:
+            if movie['title'] == _title:
+                return movie
+        raise GraphQLError("bad input parameter")
+
 def update_movie_rate(_,info,_id,_rate):
     newmovies = {}
     newmovie = {}
@@ -18,7 +30,50 @@ def update_movie_rate(_,info,_id,_rate):
                 newmovie = movie
                 newmovies = movies
     with open('{}/data/movies.json'.format("."), "w") as wfile:
-        json.dump(newmovies, wfile)
+        json.dump(newmovies, wfile, indent=4)
+    return newmovie
+
+def update_movie_title(_,info,_id,_title):
+    newmovies = {}
+    newmovie = {}
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                movie['title'] = _title
+                newmovie = movie
+                newmovies = movies
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(newmovies, wfile, indent=4)
+    return newmovie
+
+def create_movie(_,info,_movie):
+    newmovies = {}
+    newmovie = {}
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        for movie in movies['movies']:
+            if movie['id'] == _movie['id']:
+                raise GraphQLError("an existing item already exists")
+        newmovie = _movie
+        newmovies = movies
+        newmovies["movies"].append(newmovie)
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(newmovies, wfile, indent=4)
+    return newmovie
+
+def delete_movie(_,info,_id):
+    newmovies = {}
+    newmovie = {}
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        newmovies = movies
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                newmovie = movie
+                newmovies["movies"].remove(newmovie)
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(newmovies, wfile, indent=4)
     return newmovie
 
 def resolve_actors_in_movie(movie, info):
