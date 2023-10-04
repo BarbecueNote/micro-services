8808dd594e638c3f5e07c200584727dda2b01cab
Julien-DAI <julien.dai@imt-atlantique.net>
2023-10-04 10:38:02 +0200

    tp 1 complété

--- UE-AD-A1-REST-main/booking/booking.py
+++ UE-AD-A1-REST-main/booking/booking.py
@@ -15,6 +15,45 @@ with open('{}/databases/bookings.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
 
+@app.route("/bookings", methods=['GET'])
+def get_json():
+    res = make_response(jsonify(bookings), 200)
+    return res
+
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_booking_for_user(userid):
+
+    for booking in bookings:
+        if str(booking["userid"]) == str(userid):
+            return make_response(jsonify(booking), 200)
+    return make_response(jsonify({"error":"bad input parameter"}),400)
+
+@app.route("/bookings/<userid>", methods=['POST'])
+def add_booking_byuser(userid):
+    req = request.get_json()
+    times = requests.get("http://" + request.host.split(':')[0] + ":3202/showtimes").json()
+    valid_date = False
+    for time in times:
+        if str(time["date"]) == str(req["date"]):
+            valid_date = True
+            if not all(item in time["movies"] for item in req["movies"]):
+                return make_response(jsonify({"error": "bad input parameter"}), 400)
+    if not valid_date:
+        return make_response(jsonify({"error": "bad input parameter"}), 400)
+    for booking in bookings:
+        if str(booking["userid"]) == str(userid):
+            for date in booking["dates"]:
+                if str(req["date"]) == str(date["date"]):
+                    for movie in date["movies"]:
+                        if movie in req["movies"]:
+                            return make_response(jsonify({"error": "an existing item already exists"}), 409)
+                    date["movies"] += req["movies"]
+                    return make_response(jsonify({"message": "Booking created"}), 200)
+            booking["dates"].append(req)
+            return make_response(jsonify({"message":"Booking created"}), 200)
+    return make_response(jsonify({"error": "bad input parameter"}), 400)
+
+
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
--- UE-AD-A1-REST-main/user/user.py
+++ UE-AD-A1-REST-main/user/user.py
@@ -15,6 +15,40 @@ with open('{}/databases/users.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
+@app.route("/users", methods=['GET'])
+def get_users():
+   res = make_response(jsonify(users),200)
+   return res
+
+@app.route("/users/<id>", methods=['GET'])
+def get_user_byid(id):
+   for user in users:
+      if user["id"] == str(id):
+         res = make_response(jsonify(user),200)
+         return res
+   return make_response(jsonify({"error": "bad input parameter"}), 400)
+
+@app.route("/users/<id>/reservations", methods=['GET'])
+def get_reservations_byid(id):
+   for user in users:
+      if user["id"] == str(id):
+         res = make_response(jsonify(requests.get("http://" + request.host.split(':')[0] + ":3201/bookings/"+id).json()),200)
+         return res
+   return make_response(jsonify({"error": "bad input parameter"}), 400)
+@app.route("/users/<id>/reservations/movies", methods=['GET'])
+def get_reservations_movies_byid(id):
+   for user in users:
+      if user["id"] == str(id):
+         reservations = requests.get("http://" + request.host.split(':')[0] + ":3201/bookings/"+id).json()
+         films= []
+         for date in reservations["dates"]:
+            for movies in date['movies']:
+               movie = requests.get("http://" + request.host.split(':')[0] + ":3200/movies/"+movies).json()
+               films.append(movie)
+         res = make_response(jsonify(films),200)
+         return res
+   return make_response(jsonify({"error": "bad input parameter"}), 400)
+
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
